731147c06b5ee3bf7b2711547b89206ad40729f6
abelsanxd <abelsan4665@gmail.com>
2024-04-29 20:07:49 -0300

    agregamos en la app

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,22 @@ import Footer from "./components/common/Footer";
 import Inicio from "./components/pages/Inicio";
 import Administrador from "./components/pages/Administrador";
 import Error404 from "./components/pages/Error404";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 
 function App() {
   return (
-    <>
-      <Menu></Menu>
-      <Inicio></Inicio>
-      <Footer></Footer>
-    </>
+   <BrowserRouter>
+   <Menu></Menu>
+   <Routes>
+    <Route exact path="/" element={<Inicio></Inicio>}></Route>
+    <Route exact path="/administrador" element={<Administrador></Administrador>}></Route>
+    <Route exact path="/login" element={<Administrador></Administrador>}></Route>
+    <Route exact path="/detalle" element={<Administrador></Administrador>}></Route>
+   </Routes>
+   <Route path="*" element={<Error404></Error404>}></Route>
+   <Footer></Footer>
+   </BrowserRouter>
   );
 }
 
