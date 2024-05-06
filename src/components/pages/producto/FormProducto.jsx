22a39f207b140c9860269ee872bae3a2e93e8559
abelsanxd <abelsan4665@gmail.com>
2024-05-06 13:13:48 -0300

    se agregaron todas las validaciones del formulario crear producto

--- src/components/pages/producto/FormProducto.jsx
+++ src/components/pages/producto/FormProducto.jsx
@@ -1,56 +1,90 @@
 import { useState } from "react";
 import { Button, Form } from "react-bootstrap";
+import { useForm } from "react-hook-form";
 
 const FormProducto = () => {
-  const [nombre, setNombre] = useState("");
-  const [precio, setPrecio] = useState("");
-  const [imagenUrl, setImagenUrl] = useState("");
-  const [categoria, setCategoria] = useState("");
-  const [descripcionBreve, setDescripcionBreve] = useState("");
-  const [descripcionAmplia, setDescripcionAmplia] = useState("");
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
+  const onSubmit = (data) => {
+    console.log(data);
   };
 
   return (
     <section className="container">
-      <Form className="my-4" onSubmit={handleSubmit}>
+      <h1 className="display-3 mt-5">Nuevo producto</h1>
+      <hr />
+      <Form className="my-4" onSubmit={handleSubmit(onSubmit)}>
         <Form.Group className="mb-3" controlId="nombre">
-          <Form.Label>Producto*</Form.Label>
+          <Form.Label>Producto</Form.Label>
           <Form.Control
             type="text"
             placeholder="Ingrese el nombre del producto"
-            value={nombre}
-            onChange={(e) => setNombre(e.target.value)}
+            {...register("nombre", {
+              required: "El nombre del producto es obligatorio",
+              minLength: {
+                value: 2,
+                message:
+                  "Debe ingresar como mínimo 2 caracteres para el nombre del producto",
+              },
+              maxLength: {
+                value: 50,
+                message:
+                  "Debe ingresar como máximo 50 caracteres para el nombre del producto",
+              },
+            })}
           />
+          {errors.nombre && <span>{errors.nombre.message}</span>}
         </Form.Group>
         <Form.Group className="mb-3" controlId="precio">
-          <Form.Label>Precio*</Form.Label>
+          <Form.Label>Precio</Form.Label>
           <Form.Control
             type="text"
             placeholder="Ingrese el precio del producto"
-            value={precio}
-            onChange={(e) => setPrecio(e.target.value)}
+            {...register("precio", {
+              required: "El precio del producto es obligatorio",
+              pattern: {
+                value: /^\d+(\.\d{1,2})?$/,
+                message: "Ingrese un precio válido",
+              },
+            })}
           />
+          {errors.precio && <span>{errors.precio.message}</span>}
         </Form.Group>
         <Form.Group className="mb-3" controlId="imagenUrl">
           <Form.Label>URL de la Imagen</Form.Label>
           <Form.Control
             type="text"
             placeholder="Ingrese la URL de la imagen del producto"
-            value={imagenUrl}
-            onChange={(e) => setImagenUrl(e.target.value)}
+            {...register("imagenUrl", {
+              required: "La URL de la imagen es obligatoria",
+              pattern: {
+                value: /^(ftp|http|https):\/\/[^ "]+$/,
+                message: "Ingrese una URL válida",
+              },
+            })}
           />
+          {errors.imagenUrl && <span>{errors.imagenUrl.message}</span>}
         </Form.Group>
         <Form.Group className="mb-3" controlId="categoria">
           <Form.Label>Categoría</Form.Label>
           <Form.Control
-            type="text"
-            placeholder="Ingrese la categoría del producto"
-            value={categoria}
-            onChange={(e) => setCategoria(e.target.value)}
-          />
+            as="select"
+            {...register("categoria", {
+              required: "La categoría del producto es obligatoria",
+            })}
+          >
+            <option value="">Seleccionar Categoría</option>
+            <option value="Infusiones">Infusiones</option>
+            <option value="Batidos">Batidos</option>
+            <option value="Dulces">Dulces</option>
+            <option value="Salado">Salado</option>
+            <option value="Sandwich">Sandwich</option>
+          </Form.Control>
+          {errors.categoria && <span>{errors.categoria.message}</span>}
         </Form.Group>
         <Form.Group className="mb-3" controlId="descripcionBreve">
           <Form.Label>Descripción Breve</Form.Label>
@@ -58,9 +92,17 @@ const FormProducto = () => {
             as="textarea"
             rows={3}
             placeholder="Ingrese una descripción breve del producto"
-            value={descripcionBreve}
-            onChange={(e) => setDescripcionBreve(e.target.value)}
+            {...register("descripcionBreve", {
+              maxLength: {
+                value: 200,
+                message:
+                  "Debe ingresar como máximo 200 caracteres para la descripción breve del producto",
+              },
+            })}
           />
+          {errors.descripcionBreve && (
+            <span>{errors.descripcionBreve.message}</span>
+          )}
         </Form.Group>
         <Form.Group className="mb-3" controlId="descripcionAmplia">
           <Form.Label>Descripción Amplia</Form.Label>
@@ -68,9 +110,17 @@ const FormProducto = () => {
             as="textarea"
             rows={5}
             placeholder="Ingrese una descripción amplia del producto"
-            value={descripcionAmplia}
-            onChange={(e) => setDescripcionAmplia(e.target.value)}
+            {...register("descripcionAmplia", {
+              maxLength: {
+                value: 500,
+                message:
+                  "Debe ingresar como máximo 500 caracteres para la descripción amplia del producto",
+              },
+            })}
           />
+          {errors.descripcionAmplia && (
+            <span>{errors.descripcionAmplia.message}</span>
+          )}
         </Form.Group>
         <Button variant="primary" type="submit">
           Crear Producto
