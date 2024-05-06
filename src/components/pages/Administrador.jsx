8d74aa9809d177e0cd32c77d90c36d3f38d02a19
abelsanxd <abelsan4665@gmail.com>
2024-05-06 05:14:26 -0300

    se agrego el formulario para agregar un prodcuto en administrador, y la route en app

--- src/components/pages/Administrador.jsx
+++ src/components/pages/Administrador.jsx
@@ -1,14 +1,28 @@
+import { useState } from "react";
 import { Button, Table } from "react-bootstrap";
+import { Link } from "react-router-dom"; // Importa Link
+import FormProducto from "../pages/producto/FormProducto.jsx";
 import ItemProducto from "./producto/ItemProducto";
+import Swal from "sweetalert2";
 
 const Administrador = () => {
+  const [mostrarModal, setMostrarModal] = useState(false);
+
+  const handleMostrarModal = () => {
+    setMostrarModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setMostrarModal(false);
+  };
+
   return (
     <section className="container mainSection">
       <div className="d-flex justify-content-between align-items-center mt-5">
         <h1 className="display-4 ">Productos disponibles</h1>
-        <Button variant="primary">
-          <i className="bi bi-file-earmark-plus"></i>
-        </Button>
+        <Link to="/crear-producto" className="btn btn-primary">
+          <i className="bi bi-file-earmark-plus"></i> Crear Producto
+        </Link>
       </div>
       <hr />
       <Table responsive striped bordered hover>
