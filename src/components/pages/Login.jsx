d61339b2d5f7e46d15acee4d5846811e1656c808
abelsanxd <abelsan4665@gmail.com>
2024-05-06 03:43:35 -0300

    se valido el password

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -1,28 +1,57 @@
 import { Container, Form, Button } from "react-bootstrap";
+import { useForm } from "react-hook-form";
 
 const Login = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
     <Container className="my-5">
       <h1 className="mb-5">Ingresa al sitio</h1>
-      <Form>
+      <Form onSubmit={handleSubmit(onSubmit)}>
         <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Correo electronico</Form.Label>
-
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Label>Correo electrónico</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Ingresa tu correo electrónico"
+            {...register("email", {
+              required: "El correo electrónico es obligatorio",
+              pattern: {
+                value: /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/,
+                message: "Por favor, ingresa un correo electrónico válido",
+              },
+            })}
+          />
+          <Form.Text className="text-danger">{errors.email?.message}</Form.Text>
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Label>Contraseña</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Ingresa tu contraseña"
+            {...register("password", {
+              required: "La contraseña es obligatoria",
+              minLength: {
+                value: 8,
+                message: "La contraseña debe tener al menos 8 caracteres",
+              },
+            })}
+          />
+          <Form.Text className="text-danger">
+            {errors.password?.message}
+          </Form.Text>
         </Form.Group>
 
-        <Form.Group className="mb-3">
-          <Form.Text className="text-danger">Algun error</Form.Text>
-        </Form.Group>
-
-        <Button variant="primary" type="success">
-          Submit
+        <Button variant="primary" type="submit">
+          Iniciar sesión
         </Button>
       </Form>
     </Container>
